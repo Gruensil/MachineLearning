11e864e0c4fcd292359d97ecb43cd9ba06075fe9
Silas Gruen <gruensil@mail.uni-paderborn.de>
2017-11-25 15:08:28 +0100

    Exercise 1

---
+++ squareAndCubeThisNumber.m
@@ -0,0 +1,7 @@
+function [outputArg1,outputArg2] = squareAndCubeThisNumber(x)
+%SQUAREANDCUBETHISNUMBER Summary of this function goes here
+%   Detailed explanation goes here
+outputArg1 = x^2;
+outputArg2 = x^3;
+end
+
